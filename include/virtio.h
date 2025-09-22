b5b59dea4b8f8b3e2e9fc67bb9f6897fced3e373
Adam Lackorzynski <adam@l4re.org>
2025-09-22 00:00:00 +0000

    Add CAN and Watchdog Virtio-IDs
    Signed-off-by: Adam Lackorzynski <adam@l4re.org>
    
    Change-Id: Iad457355349e46355eb46cd976e411f2e30d99f8

--- include/virtio.h
+++ include/virtio.h
@@ -78,6 +78,8 @@ enum L4virtio_device_ids
   L4VIRTIO_ID_FS            = 26,     /**< FS */
   L4VIRTIO_ID_SCMI          = 32,     /**< Scmi device */
   L4VIRTIO_ID_I2C           = 34,     /**< I2C device */
+  L4VIRTIO_ID_WATCHDOG      = 35,     /**< Watchdog device */
+  L4VIRTIO_ID_CAN           = 36,     /**< CAN device */
   L4VIRTIO_ID_GPIO          = 41,     /**< Gpio device */
 
   L4VIRTIO_ID_SOCK          = 0x9999, /**< Unofficial socket device. */
